d88dfac71fcf05eafb87459a0beffc34066eb20c
Tobiasz <tobiaszkubiak31@gmail.com>
2020-03-09 18:27:27 +0100

    Create Product class(using Money as composition)

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Product.java
@@ -0,0 +1,64 @@
+package pl.com.bottega.ecommerce.sales.domain.offer;
+
+import java.util.Date;
+
+public class Product {
+  private String productId;
+
+  private Money productPrice;
+
+  private String productName;
+
+  private Date productSnapshotDate;
+
+  private String productType;
+
+  public Product(String productId, Money productPrice, String productName,
+      Date productSnapshotDate, String productType) {
+    this.productId = productId;
+    this.productPrice = productPrice;
+    this.productName = productName;
+    this.productSnapshotDate = productSnapshotDate;
+    this.productType = productType;
+  }
+
+  public String getProductId() {
+    return productId;
+  }
+
+  public void setProductId(String productId) {
+    this.productId = productId;
+  }
+
+  public Money getProductPrice() {
+    return productPrice;
+  }
+
+  public void setProductPrice(Money productPrice) {
+    this.productPrice = productPrice;
+  }
+
+  public String getProductName() {
+    return productName;
+  }
+
+  public void setProductName(String productName) {
+    this.productName = productName;
+  }
+
+  public Date getProductSnapshotDate() {
+    return productSnapshotDate;
+  }
+
+  public void setProductSnapshotDate(Date productSnapshotDate) {
+    this.productSnapshotDate = productSnapshotDate;
+  }
+
+  public String getProductType() {
+    return productType;
+  }
+
+  public void setProductType(String productType) {
+    this.productType = productType;
+  }
+}
